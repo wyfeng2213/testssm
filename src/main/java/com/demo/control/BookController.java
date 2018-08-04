ed677a192515f866bdb81c48066b10ff9869166b
wyfeng2213 <874999821@qq.com>
2018-08-04 11:46:42 +0800

    提交apidoc的注释生成文档

--- src/main/java/com/demo/control/BookController.java
+++ src/main/java/com/demo/control/BookController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONObject;
 import com.demo.dto.AppointExecution;
 import com.demo.dto.Result;
 import com.demo.entity.Book;
@@ -20,6 +22,10 @@ import com.demo.enums.AppointStateEnum;
 import com.demo.exception.NoNumberException;
 import com.demo.exception.RepeatAppointException;
 import com.demo.service.BookService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import io.github.yedaxia.apidocs.ApiDoc;
 
 @Controller
 @RequestMapping("/book") // url:/模块/资源/{id}/细分 /seckill/list
@@ -31,14 +37,42 @@ public class BookController {
 	private BookService bookService;
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	private String list(Model model) {
+	private String list(Model model,@RequestBody JSONObject obj) {
+
+		PageHelper.startPage(1, 10,true);
+
 		List<Book> list = bookService.getList();
 		model.addAttribute("list", list);
+
+		PageInfo<Book> pageInfo = new PageInfo<>(list);
 		// list.jsp + model = ModelAndView
 		return "list";// WEB-INF/jsp/"list".jsp
 	}
 
+	/**
+	 * 
+	 * @api {get} /company/list 获取公司信息
+	 * @apiName 获取公司列表
+	 * @apiGroup All
+	 * @apiVersion 0.1.0
+	 * @apiDescription 接口详细描述
+	 * 
+	 * @apiParam {int} pageNum分页大小
+	 * 
+	 * @apiSuccess {String} code 结果码
+	 * @apiSuccess {String} msg 消息说明
+	 * @apiSuccess {Object} data 分页数据封装
+	 * @apiSuccess {int} data.count 总记录数
+	 * @apiSuccess {Object[]} data.list 分页数据对象数组
+	 * @apiSuccessExample Success-Response: HTTP/1.1 200 OK { code:0, msg:'success',
+	 *                    data:{} }
+	 * 
+	 * @apiError All 对应<code>id</code>的用户没找到 asdfasdf
+	 * @apiErrorExample {json} Error-Response: HTTP/1.1 404 Not Found { code:1,
+	 *                  msg:'user not found', }
+	 */
 	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
+	@ApiDoc(Book.class)
 	private String detail(@PathVariable("bookId") Long bookId, Model model) {
 		if (bookId == null) {
 			return "redirect:/book/list";
